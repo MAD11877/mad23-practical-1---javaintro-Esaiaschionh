f3748884d21a073a9e48720be70517786bd8c93e
Esaiaschionh <115963425+Esaiaschionh@users.noreply.github.com>
2023-04-20 11:29:21 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -6,15 +6,15 @@ public class Question2
     Scanner in = new Scanner(System.in);
     
     //Height prompt
-    System.out.println("Enter height (m): "); 
+    //System.out.println("Enter height (m): "); 
     double height = in.nextDouble();
 
     //Weight prompt
-    System.out.println("Enter weight (Kg): ");
+    //System.out.println("Enter weight (Kg): ");
     double weight = in.nextDouble();
 
     //BMI calculation & output
     double bmi = weight/(height * height);
-    System.out.println("Your BMI is "+ bmi);
+    System.out.println(bmi);
   }
 }
