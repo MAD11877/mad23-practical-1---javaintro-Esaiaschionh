faa38822e8ddfa60506f67f046822e2ba0fc10da
Esaiaschionh <115963425+Esaiaschionh@users.noreply.github.com>
2023-04-20 11:48:55 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -4,8 +4,9 @@ public class Question3
 {
   public static void main(String[] args)
   {
-    Int number = in.nextInt();
-    Integer answer= number * number ;
+    int num;
+    int number = in.nextInt();
+    integer answer= number * number ;
     System.out.println(answer);
     Scanner in = new Scanner(System.in);
     
