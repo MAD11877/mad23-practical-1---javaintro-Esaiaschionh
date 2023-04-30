af3d268e65b40db679c44413dbb29b04d3e52978
Esaiaschionh <115963425+Esaiaschionh@users.noreply.github.com>
2023-04-30 08:49:59 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -4,11 +4,12 @@ public class Question3
 {
   public static void main(String[] args)
   {
+    Scanner in = new Scanner(System.in);
     int num;
     int num = in.nextInt();
     int answer = num * num ;
     System.out.println(answer);
-    Scanner in = new Scanner(System.in);
+   
     
   }
 }
