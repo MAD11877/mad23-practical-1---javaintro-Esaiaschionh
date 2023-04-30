8b20b11a30464967b85cf9c5a53fe906d8d2382c
Esaiaschionh <115963425+Esaiaschionh@users.noreply.github.com>
2023-04-30 10:19:51 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -1,4 +1,10 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
 
 public class Question5
 {
