7313ea56bb79aa76db2c560b2a1b672e7db09d2a
Esaiaschionh <115963425+Esaiaschionh@users.noreply.github.com>
2023-04-30 10:17:14 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -4,29 +4,36 @@ public class Question5
 {
   public static void main(String[] args)
   {
-    /**
-     * Prompt the user for number. This input indicates the number of integers the user will be entering next. 
-     * Print out the mode (highest occurrence) from the set of integers. 
-     *    e.g.
-     *     > 5
-     *     > 2
-     *     > 4
-     *     > 1
-     *     > 3
-     *     > 4
-     *     4
-     * 
-     *    e.g.
-     *     > 4
-     *     > 2
-     *     > 2
-     *     > 3
-     *     > 3
-     *     2
-     * Hint: Use a loop to get input. Use another 2 loops to find the mode
-     */
-     
     Scanner in = new Scanner(System.in);
+    //System.out.println("Enter number: "); 
+    int num;
+    num = in.nextInt();
+    List<Integer> numbers = new ArrayList<>();
+    do{
+      //System.out.print(">");
+      int a = in.nextInt();
+      numbers.add(a);
+      num--;
+    }while (num>0);
+    
+    Map<Integer, Integer> frequency = new HashMap<>();
+    for (int n : numbers) {
+      if (frequency.containsKey(n)) {
+        frequency.put(n, frequency.get(n) + 1);
+      } else {
+      frequency.put(n, 1);
+      }
+    }
+
+    int mode = 0;
+    int maxFrequency = 0;
+    for (Entry<Integer, Integer> entry :  frequency.entrySet()) {
+      if (entry.getValue() > maxFrequency) {
+        mode = entry.getKey();
+        maxFrequency = entry.getValue();            
+      }
+    }
+    System.out.println(mode);
     
   }
 }
